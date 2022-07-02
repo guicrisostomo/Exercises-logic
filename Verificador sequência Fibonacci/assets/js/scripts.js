4593d2c1759d90a37f73091439f986eb640bd7e8
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-07-01 21:21:29 -0300

    Add: file style and new elements

--- Verificador sequência Fibonacci/assets/js/scripts.js
+++ Verificador sequência Fibonacci/assets/js/scripts.js
@@ -1,34 +1,60 @@
-function informaNumero() {
-    let numeroInformado;
+document.getElementById('buttonCheck').addEventListener('click', () => {
+    let numInfo;
+    let divYes = document.getElementById('divYes');
+    let divNo = document.getElementById('divNo');
 
-    numeroInformado = Number(prompt("Informe um número:"));
+    numInfo = parseInt(document.getElementById('textNumber').value)
 
-    if (calculaSequencia(numeroInformado)) {
-        console.log("Este número pertence a sequência de Fibonacci!");
+    if (calculaSequencia(numInfo)) {
+        divYes.style.display = 'flex';
+        divNo.style.display = 'none';
     } else {
-        console.log("Este número NÃO pertence a sequência de Fibonacci!");
+        divYes.style.display = 'none';
+        divNo.style.display = 'flex';
     }
-}
-
-function calculaSequencia(numInfo) {
-    if (!numInfo) return false;
     
-    let ultimoValor, penultimoValor, valor;
-    penultimoValor = 0;
-    ultimoValor = 1;
+    flashesDiv();
+});
+
+function calculaSequencia(numInfoParam) {
+    if (!numInfoParam) return false;
+    
+    let lastValue, penultimateValue, value;
+    penultimateValue = 0;
+    lastValue = 1;
 
     do {
-        valor = penultimoValor + ultimoValor;
+        value = penultimateValue + lastValue;
 
-        if (valor === numInfo) {
+        if (value === numInfoParam) {
             return true;
         }
 
-        penultimoValor = ultimoValor;
-        ultimoValor = valor;
-    } while (numInfo >= valor);
+        penultimateValue = lastValue;
+        lastValue = value;
+    } while (numInfoParam >= value);
 
     return false;
 }
 
-informaNumero();
+function flashesDiv() {
+    setInterval(function() {
+        let divYes = document.getElementById('divYes');
+        let divNo = document.getElementById('divNo');
+    
+        if (divYes.style.display === 'flex') {
+            if (divYes.children[0].style.backgroundColor === 'green') {
+                divYes.children[0].style.backgroundColor = '#023b02';
+            } else {
+                divYes.children[0].style.backgroundColor = 'green';
+            }
+        } else if (divNo.style.display === 'flex') {
+            if (divNo.children[0].style.backgroundColor === 'red') {
+                divNo.children[0].style.backgroundColor = '#840303';
+            } else {
+                divNo.children[0].style.backgroundColor = 'red';
+            }
+        }
+    
+    }, 1500);
+}
